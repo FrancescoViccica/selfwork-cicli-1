fa8c4c77476324eda07042d78eac0c9785722473
Francesco Viccica <viccicafrancesco@gmail.com>
2025-03-05 11:38:37 +0100

    esercizio svolto

---
+++ main.js
@@ -0,0 +1,16 @@
+let num = 2
+
+// console.log(num);
+
+
+for (let i = 0 ; i <= 10; i++){
+
+    // console.log(i);
+
+    tab = i * num;
+
+    console.log(tab);
+    
+}
+
+console.log(`tabellina terminata`);
